2a605e3efa7bea6c53698b4f401fd3b990dcdded
Uveshbahelim <uveshbahelim622@gmail.com>
2025-04-27 23:56:11 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import CurrencyConverter from "./CurrencyConverter/CurrencyConverter";
+import Home from "./Home";
+import Todo from "./Todo/Todo";
+
+
+export {Todo,CurrencyConverter,Home}
---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route,RouterProvider } from "react-router-dom"
+import {Todo,CurrencyConverter,Home} from "./components/index.js"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      <Route path='/' element={<Home/>}/>
+      <Route path='todo-app' element={<Todo/>}/>
+      <Route path='currency-converter' element={<CurrencyConverter/>}/>
+    </Route>
+  )
+)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
