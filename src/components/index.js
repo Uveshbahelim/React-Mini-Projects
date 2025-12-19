d922869104e1e88d55428f588bffd39e1e9d36ee
Uveshbahelim <uveshbahelim622@gmail.com>
2025-12-19 19:03:17 +0530

    New Project "Notes App"

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,6 @@ import ExpenseTracker from "./ExpenseTracker/ExpenseTracker";
 import Home from "./Home";
 import Todo from "./Todo/Todo";
 import EcommercePage from "./EcommercePage/ProductPage"
-import NotesApp from "../NotesApp/NotesApp";
+import NotesApp from "./NotesApp/NotesApp";
 
 export { Todo, CurrencyConverter, ExpenseTracker, EcommercePage, NotesApp, Home };
